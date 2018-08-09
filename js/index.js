114864565b67dd803ac32c262fa189a08532abdc
Josh Dean <josdea@gmail.com>
2018-08-09 15:37:31 -0400

    added support for a styles.css file

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ new Vue({
     input: `Use the box in the far left to try out your markdown. Data is saved in your local browser storage.
 
 # Markdown Reference Guide
-Download the HTML when you're finished.  The HTML file comes with support for math as well as Bootstrap 4 styling and functionality.
+Download the HTML when you're finished.  The HTML file comes with support for math as well as Bootstrap 4 styling and functionality.  It also references a styles.css if available. 
 
 ## Emphasis
 *Italics* or _Italics_
@@ -129,7 +129,7 @@ This is math in block format on its own line. \\\\[y=4x^2 - 2x=13\\\\]
         pTitle = "";
       }
  
-      let tStart = '<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_HTMLorMML"></script><title>' + pTitle + '</title><style></style></head><body><main><div class="container">';
+      let tStart = '<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_HTMLorMML"></script><title>' + pTitle + '</title><link rel="stylesheet" type="text/css" href="styles.css"><style></style></head><body><main><div class="container">';
       let tEnd = '</div></main><script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script></body></html>';
       let text = tStart + this.rawHTML + tEnd;
       // let d = new Date();
